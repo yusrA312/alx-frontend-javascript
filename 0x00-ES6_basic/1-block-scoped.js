c2b312929b393a88e41248853b11a5bc99c56d65
yusrA312 <yusra2yahya@gmail.com>
2024-06-24 10:59:00 -0700

    done

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,9 +1,9 @@
-export default function taskBlock(trueOrFalse) { 
+export default function taskBlock(trueOrFalse) {
   const task = false;
   const task2 = true;
 
   if (trueOrFalse) {
-
+    // This block is now empty as the variables were not contributing to the return value
   }
 
   return [task, task2];
