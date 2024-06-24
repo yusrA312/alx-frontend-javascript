b0b196659327d2c0cdaade40068705a61bcec190
yusrA312 <yusra2yahya@gmail.com>
2024-06-24 05:59:32 -0700

    done

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,7 +1,7 @@
 export default function taskBlock(trueOrFalse) {
  
-  const task = false;
-  const task2 = true;
+  var task = false;
+  var task2 = true;
 
   if (trueOrFalse) {
     const task = true;
