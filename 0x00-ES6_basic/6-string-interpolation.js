b4d4bd9280516e0f81502bc011c515ef2d6dbdfe
yusrA312 <yusra2yahya@gmail.com>
2024-06-24 07:01:16 -0700

    done

--- 0x00-ES6_basic/6-string-interpolation.js
+++ 0x00-ES6_basic/6-string-interpolation.js
@@ -5,9 +5,5 @@ export default function getSanFranciscoDescription() {
     gdp: '$154.2 billion',
     capita: '$178,479',
   };
-
-  return 'As of {0}, it was the seventh-highest income county in the United States, ' +
-         'with a per capita personal income of {1}. As of 2015, San Francisco ' +
-         'proper had a GDP of {2}, and a GDP per capita of {3}.'
-         .format(year, budget.income, budget.gdp, budget.capita);
+  return `As of ${year}, it was the seventh-highest income county in the United States, with a per capita personal income of ${budget.income}. As of 2015, San Francisco proper had a GDP of ${budget.gdp}, and a GDP per capita of ${budget.capita}.`;
 }
