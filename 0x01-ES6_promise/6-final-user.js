e1b542c3892e00ffe105e2bd75ddf33a556f79b1
yusrA312 <yusra2yahya@gmail.com>
2024-06-25 09:46:49 -0700

    done

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,9 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  const signUpPromise = signUpUser(firstName, lastName);
+  const uploadPromise = uploadPhoto(fileName);
+
+  
+}
