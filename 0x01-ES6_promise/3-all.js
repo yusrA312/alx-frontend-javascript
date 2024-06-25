fd763fffbcd22fde068d71079a30876d80ffff03
yusrA312 <yusra2yahya@gmail.com>
2024-06-25 08:43:32 -0700

    done

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,13 +1,13 @@
-import { uploadPhoto, createUser } from "./utils";
+import { uploadPhoto, createUser } from './utils';
 
 function handleProfileSignup() {
-    Promise.all([uploadPhoto(), createUser()])
-        .then(([photo, user]) => {
-            console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
-        })
-        .catch(error => {
-            console.error('Signup system offline');
-        });
+  Promise.all([uploadPhoto(), createUser()])
+    .then(([photo, user]) => {
+      console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
+    })
+    .catch(() => {
+      console.log('Signup system offline');
+    });
 }
 
 export default handleProfileSignup;
