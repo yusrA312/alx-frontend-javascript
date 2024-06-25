69fd56acfd9a20f816b41508bc59c08fa84af6c7
yusrA312 <yusra2yahya@gmail.com>
2024-06-25 11:59:12 -0700

    done

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -2,11 +2,14 @@ export default function guardrail(mathFunction) {
   const queue = [];
 
   try {
+    // Execute the mathFunction and append the result to the queue
     const result = mathFunction();
     queue.push(result);
   } catch (error) {
-    queue.push(error.message);
+    // If an error is thrown, append the error message to the queue
+    queue.push(`Error: ${error.message}`);
   } finally {
+    // Append the guardrail message to the queue in all cases
     queue.push('Guardrail was processed');
   }
 
