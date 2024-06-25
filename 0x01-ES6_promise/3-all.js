c50b656bbe896e0d0bc045e7051631bc64eefcf0
yusrA312 <yusra2yahya@gmail.com>
2024-06-25 08:58:22 -0700

    done

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -2,8 +2,7 @@ import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
-    .then((responses) => {
-      const [photoResponse, userResponse] = responses;
+    .then(([photoResponse, userResponse]) => {
       console.log(`${photoResponse.body} ${userResponse.firstName} ${userResponse.lastName}`);
     })
     .catch(() => {
