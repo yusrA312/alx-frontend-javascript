9754fedea41704f0c603d30eb8c2de020f0baa29
yusrA312 <yusra2yahya@gmail.com>
2024-06-27 05:24:58 -0700

    done

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,6 +1,10 @@
 export default class Building {
   constructor(sqft) {
+	   if (new.target === Building) {
+            throw new Error("Building is an abstract class and cannot be instantiated directly.");
+        }
     this._sqft = sqft;
+
   }
 
   get sqft() {
