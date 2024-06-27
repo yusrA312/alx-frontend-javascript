f76b3a0617d5ddcc8a49ec41e77e432fdf986e73
yusrA312 <yusra2yahya@gmail.com>
2024-06-27 08:00:14 -0700

    done

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -1,21 +1,16 @@
-// Importing Building from a relative path
 import Building from './5-building';
 
-// Extending the Building class
 export default class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
     super(sqft); // Call the parent class constructor
     this._floors = floors; // Set the _floors property
   }
 
-  // Getter for floors
   get floors() {
     return this._floors;
   }
 
-  // Method to provide evacuation warning message
   evacuationWarningMessage() {
     return `Evacuate slowly the ${this.floors} floors`; // Access floors via getter
   }
 }
-
