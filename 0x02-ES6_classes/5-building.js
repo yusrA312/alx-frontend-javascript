8ad1faba1655fb4d4964c9fa9d3c54d7cedde820
yusrA312 <yusra2yahya@gmail.com>
2024-06-27 04:44:47 -0700

    done

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,20 +1,13 @@
-class Building {
-    constructor(sqft) {
-        if (new.target === Building) {
-            throw new Error("Building is an abstract class and cannot be instantiated directly.");
-        }
-        this._sqft = sqft;
-    }
+export default class Building {
+  constructor(sqft) {
+    this._sqft = sqft;
+  }
 
-    // Getter for sqft attribute
-    get sqft() {
-        return this._sqft;
-    }
+  get sqft() {
+    return this._sqft;
+  }
 
-    // Method to check if evacuationWarningMessage is implemented
-    evacuationWarningMessage() {
-        throw new Error("Class extending Building must override evacuationWarningMessage");
-    }
+  static evacuationWarningMessage() {
+    throw new Error('Class extending Building must override evacuationWarningMessage');
+  }
 }
-
-export default Building;
